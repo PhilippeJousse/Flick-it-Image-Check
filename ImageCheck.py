8012c62896f5ab8b02c1832a6de94eeab1c602ec
Philicreep <Philippejousse30@gmail.com>
2022-11-25 08:47:16 +0100

    firestore bug fix

--- ImageCheck.py
+++ ImageCheck.py
@@ -1,16 +1,16 @@
-import os
-import json
+import os, firebase_admin
 from tabnanny import check
-from fastapi import FastAPI
+from firebase_admin import credentials,firestore
 from google.cloud import pubsub_v1, storage, vision
 from concurrent.futures import TimeoutError
-import firebase_admin
-from firebase_admin import credentials,firestore
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "google_key.json"
 
 firebase_admin.initialize_app(credentials.Certificate('serviceAccountCredentials.json'))
 db = firestore.client()
+
+publisher = pubsub_v1.PublisherClient()
+topic_path ="projects/third-essence-365119/topics/launch-scoring"
 
 def Vision(uri):
     client = vision.ImageAnnotatorClient()
@@ -23,14 +23,6 @@ def Vision(uri):
     for object_ in objects:
         element.append(str.lower(object_.name))
     return element
-
-def launchVision(id):
-    data = db.collection('metadata').document(id).get()
-    data = data.to_dict()
-    uri = data["uri"]
-    word = data["word"]
-    user = data["userId"]
-    return checkWord(uri,word)
     
 def checkWord(uri,word):
     result = Vision(uri)
@@ -39,27 +31,32 @@ def checkWord(uri,word):
             return 200
     return 400
 
+def getData(id):
+    data = db.collection('metadata').document(id).get()
+    data = data.to_dict()
+    return data
+
 subscriber = pubsub_v1.SubscriberClient()
 
 storage_client = storage.Client()
-bucket_name ="storage_image_api"
-bucket = storage_client.get_bucket(bucket_name)
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     id = message.data.decode("utf-8")
-    code = launchVision(id)
+    message.ack()
+    data = getData(id)
+    code = checkWord(data["uri"],data["word"])
     if(code == 200):
             print("Trouve")
             db.collection('metadata').document(id).update({"status":"True"})
-            message.ack()
+            id = id.encode('utf-8')
+            publisher.publish(topic_path,id)
             return 200
     print("Pas Trouve")
     db.collection('metadata').document(id).update({"status":"True"})
-    message.ack()
     return 400
 
-streaming_pull_future = subscriber.subscribe("projects/third-essence-365119/subscriptions/test-pubsub", callback=callback)
-print(f"Listening for messages on projects/third-essence-365119/subscriptions/test-pubsub..\n")
+streaming_pull_future = subscriber.subscribe("projects/third-essence-365119/subscriptions/launch-vision-sub", callback=callback)
+print(f"Listening for messages on projects/third-essence-365119/subscriptions/launch-vision-sub..\n")
 
 # Wrap subscriber in a 'with' block to automatically call close() when done.
 with subscriber:
